3bb592c645da0f7ba1d67e7bde3a00ed5bde056b
TraxBox Developers <39349950+TraxBox@users.noreply.github.com>
2019-09-19 18:25:08 -0400

    Artist Profile
    The artist Profile VC allows users to input data for their profile page on the app. They input Name, bio, genre etc.  This infos uploaded to a Dynamo DB database.

---
+++ artistProfileVC.swift
@@ -0,0 +1,131 @@
+//
+//  artistProfileVC.swift
+//  new_tbx
+//
+//  Created by SEVENHORNS ADMIN on 8/4/19.
+//  Copyright © 2019 TraxBox, INC. All rights reserved.
+//
+
+import UIKit
+import AWSCore
+import AWSMobileClient
+import AWSAppSync
+import Photos
+
+class artistProfileVC: UITableViewController,  UINavigationControllerDelegate, UIImagePickerControllerDelegate {
+    
+    
+   
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var firstName: UITextField!
+    @IBOutlet weak var lastName: UITextField!
+    @IBOutlet weak var stageName: UITextField!
+    
+ 
+    @IBOutlet weak var genre: UITextField!
+    @IBOutlet weak var hometown: UITextField!
+    @IBOutlet weak var bio: UITextField!
+    
+    var imagePicker = UIImagePickerController()
+    var userCognitoId = userAPI.getuserId
+    var appSyncClient: AWSAppSyncClient?
+//    var picker = UIImagePickerController()
+    
+     var _firstName: String?
+    
+    fileprivate let s3Service: S3Service = S3Service()
+  
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+//        self.view.backgroundColor = UIColor(red: 100/255, green: 100/255, blue: 100/255, alpha: 1)
+        
+//        tableView.delegate = self
+//        tableView.dataSource = self
+//        
+//        let headerView = UIView()
+//        headerView.backgroundColor = UIColor.blue
+//        headerView.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: 50)
+        
+        
+        _ = userCognitoId
+      
+//        firstName.delegate = (self as! UITextFieldDelegate);
+//        lastName.delegate = self
+//        stageName.delegate = self
+//        genre.delegate = self
+//        hometown.delegate = self
+//        bio.delegate = self
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        appSyncClient = appDelegate.appSyncClient
+        
+        
+        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(artistProfileVC.imageViewTapped(gesture:)))
+        profileImageView.addGestureRecognizer(tapGesture)
+        profileImageView.isUserInteractionEnabled = true
+        imagePicker.delegate = self
+        imagePicker.sourceType = .savedPhotosAlbum
+        imagePicker.allowsEditing = true
+    }
+
+    
+    @objc func imageViewTapped(gesture: UIGestureRecognizer) {
+        //gesture.view as? UIImageView
+        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum){
+            present(imagePicker, animated: true, completion: nil)
+        }
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        print("Cancelled picking image")
+        picker.dismiss(animated: true) {
+            //nothing to do
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        let chosenImage = info[UIImagePickerController.InfoKey.originalImage] as! UIImage
+        profileImageView.contentMode = .scaleAspectFit
+        profileImageView.image = chosenImage
+        picker.dismiss(animated: true) {
+            
+        }
+    }
+    
+    // Retrieve your Amazon Cognito ID
+  
+    
+    
+    func runMutation(){
+        
+        let mutationInput = UpdateArtistProfileInput(id:"\(userCognitoId)", firstName: "\(firstName.text!)", lastName: "\(lastName.text!)", stageName: "\(stageName.text!)", bio:"\(bio.text!)", genre: "\(genre.text!)", hometown:"\(hometown.text!)")
+        appSyncClient?.perform(mutation: UpdateArtistProfileMutation(input: mutationInput)) { (result, error) in
+            if let error = error as? AWSAppSyncClientError {
+                print("Error occurred: \(error.localizedDescription )")
+            }
+            if let resultError = result?.errors {
+                print("Error saving the item on server: \(resultError)")
+                return
+            }
+            print(result.debugDescription)
+        }
+
+    
+    
+    //        dismiss(animated: true, completion: nil)
+    
+
+        
+}
+
+    @IBAction func saveProfileTapped(_ sender: Any) {
+        s3Service.uploadData(data: (profileImageView.image?.pngData()!)!)
+        self.runMutation()
+    }
+   
+    }
+
